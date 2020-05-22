4fe30d0cc14e01f4f2676b283c9d00835843fd98
Daniel <42676493+blndr83@users.noreply.github.com>
2020-05-22 21:40:52 +0200

    clean up Books razor file by using code behind file
    clean up Books razor file by using code behind file

---
+++ Buecherverwaltung/Client/Pages/Books.razor.cs
@@ -0,0 +1,40 @@
+﻿using Buecherverwaltung.Client.Models;
+using Buecherverwaltung.Client.Services;
+using Buecherverwaltung.Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Buecherverwaltung.Client.Pages
+{
+    public class BooksBase : ComponentBase
+    {
+        [Inject]
+        HttpService HttpService { get; set; }
+        protected EditBookViewModel editBookViewModel;
+        protected bool showEditTitleDialog;
+        protected BookViewModel bookViewModel;
+
+        protected async override Task OnInitializedAsync()
+        {
+            bookViewModel = new BookViewModel(HttpService);
+            await bookViewModel.InitBooks();
+        }
+
+        protected void ShowEditTileDialog(Book book)
+        {
+            editBookViewModel = new EditBookViewModel(HttpService)
+            {
+                Book = book,
+                OnButtonClick = OnEditTitleDialogButtonClick
+            };
+            showEditTitleDialog = true;
+        }
+
+        protected void OnEditTitleDialogButtonClick()
+        {
+            editBookViewModel = null;
+            showEditTitleDialog = false;
+            StateHasChanged();
+        }
+    }
+}
